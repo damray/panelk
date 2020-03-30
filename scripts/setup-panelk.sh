ebcd72e6189bda9c9cb5e3a6e512af6de810d1ce
damray <damien.raynal@gmail.com>
2020-03-30 17:21:07 +0200

    m

--- scripts/setup-panelk.sh
+++ scripts/setup-panelk.sh
@@ -2,21 +2,30 @@
 
 set -euo pipefail
 
+user=paloalto
+
 es_url=http://elastic:${ELASTIC_PASSWORD}@192.168.45.100:9200
 kb_url=http://elastic:${ELASTIC_PASSWORD}@192.168.45.100:5601
 # Wait for Elasticsearch to start up before doing anything.
 until curl -s $es_url -o /dev/null; do
     sleep 1
 done
-
-# Set the password for the elastic user.
-until curl -s -H 'Content-Type:application/json' \
-     -XPOST $es_url/_security/user/elastic/_password \
-     -d "{\"password\": \"${ELASTIC_PASSWORD}\"}"
-do
-    sleep 2
-    echo Retrying1...
+until curl -s $kb_url -o /dev/null; do
+    sleep 1
 done
+# create user 
+#until curl -s -H 'Content-Type:application/json' \
+#     -XPOST $es_url/_security/user/${user} \
+#     -d "{\"password\": \"${ELASTIC_PASSWORD}\"}"
+#do
+# Set the password for the elastic user.
+#until curl -s -H 'Content-Type:application/json' \
+#     -XPOST $es_url/_security/user/${user}/_password \
+#     -d "{\"password\": \"${ELASTIC_PASSWORD}\"}"
+#do
+#    sleep 2
+#    echo Retrying1...
+#done
 
 until curl \
      -H 'kbn-xsrf:true' \
