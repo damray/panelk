01d7d9442764d661e52a71e76f2f478080dbf77e
damray <damien.raynal@gmail.com>
2020-03-30 18:01:43 +0200

    m

--- scripts/setup-panelk.sh
+++ scripts/setup-panelk.sh
@@ -13,13 +13,25 @@ ping -c 5 elasticsearch
 ping -c 5 kibana
 
 
+until curl -s -f http://elastic:${ELASTIC_PASSWORD}@elasticsearch:9200/_cat/health; do
+    sleep 2 
+done
+
+until curl -s -f http://elastic:${ELASTIC_PASSWORD}@kibana:5601/status; do
+    sleep 2 
+done
+
+# if [[ $$? == 52 ]]; then echo 0; else echo 1; fi
+
+
 # Wait for Elasticsearch to start up before doing anything.
-until curl -s $es_url -o /dev/null; do
+until curl -s $es_url/ -o /dev/null; do
     sleep 1
 done
 until curl -s $kb_url -o /dev/null; do
     sleep 1
 done
+
 # create user 
 #until curl -s -H 'Content-Type:application/json' \
 #     -XPOST $es_url/_security/user/${user} \
