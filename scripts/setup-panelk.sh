dee73196ac4b83e019ef96e7e568787e622f1797
damray <damien.raynal@gmail.com>
2020-04-30 11:47:01 +0200

    script update

--- scripts/setup-panelk.sh
+++ scripts/setup-panelk.sh
@@ -6,7 +6,7 @@ kb_url=http://elastic:${ELASTIC_PASSWORD}@kibana:5601
 
 # Wait for Elasticsearch to start up before doing anything.
 
-until curl -s -f http://elastic:${ELASTIC_PASSWORD}@$es_url:9200/_cat/nodes?v&pretty -o /dev/null ; do
+until curl -s -f $es_url/_cat/nodes?v&pretty -o /dev/null ; do
     sleep 2 
 done
 
